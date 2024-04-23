ddc5ee5610cc44aa04aa0f9391b57c307c212374
Ivana Spasova <zlwf168@live.rhul.ac.uk>
2024-04-23 14:51:11 +0300

    changes

--- falling-src/boundary.js
+++ falling-src/boundary.js
@@ -1,28 +1,29 @@
 class Boundary {
     constructor(x, y, w, h) {
-        let options = {
-            friction: 0.3, // Friction of the popcorn
-            restitution: 0.6, // Restitution
-            isStatic: true,
-// Angle is PI / 4
-        };
-        this.body = Bodies.circle(x, y, w, h, options);
-        this.w = w;
-        this.h = h;
-        World.add(world, this.body);
+      let options = {
+        friction: 0.3,
+        restitution: 0.6,
+        isStatic: true,
+  //      angle: PI / 4
+      };
+      this.body = Bodies.rectangle(x, y, w, h, options);
+      this.w = w;
+      this.h = h;
+      World.add(world, this.body);
     }
-
+  
     show() {
-        let pos = this.body.position; // Position of the falling popcorn
-        let angle = this.body.angle; // Angle of falling
-        push();
-        translate(pos.x, pos.y);
-        rotate(angle); // The popcorns are rotating
-        rectMode(CENTER);
-        strokeWeight(1); // Stroke of the popcorn is thin
-        stroke(0, 0, 102); // Color of the stroke - Dark Blue
-        fill(255, 255, 188); // Color of popcorn - Yellow
-        rect(0, 0, this.d);
-        pop();
+      let pos = this.body.position;
+      let angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(1);
+      noStroke();
+      fill(255,0,0);
+      rect(0, 0, this.w, this.h);
+      pop();
     }
-}
+  }
